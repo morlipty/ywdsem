92790ae0641aefed16db4717b41c6ca295a9f4b7
morlipty <morlipty@proton.me>
2025-10-12 19:14:42 +0400

    Backup 2025:10:12-19:14

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -6,8 +6,10 @@ if status is-interactive
   fzf --fish | source
   atuin init fish | source
   starship init fish | source
+  set ZELLIJ_AUTO_EXIT true
+  eval (zellij setup --generate-auto-start fish | string collect)
 end
 
-mise activate fish | source
 thefuck --alias | source
+mise activate fish | source
 zoxide init --cmd cd fish | source

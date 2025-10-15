61b44517c55ffcfc8a0df69976c9417b9690c15a
morlipty <morlipty@proton.me>
2025-10-15 16:29:10 +0400

    Backup 2025:10:15-16:29

--- dotfiles/.config/fish/conf.d/aliases.fish
+++ dotfiles/.config/fish/conf.d/aliases.fish
@@ -11,8 +11,8 @@ alias reflector-update 'sudo reflector --verbose --country Iran,Germany,France -
 alias pacunlock 'sudo rm -i /var/lib/pacman/db.lck'
 
 #music
-alias musicsampleratecheck 'find ~/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate,bit_depth -of csv=p=0 {} \; | sort | uniq -c'
-alias musicalbumartrescheck 'find ~/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 {} \; | sort | uniq -c'
+alias musicsampleratecheck 'find ~/Downloads/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate,bit_depth -of csv=p=0 {} \; | sort | uniq -c'
+alias musicalbumartrescheck 'find ~/Downloads/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 {} \; | sort | uniq -c'
 
 #backup
 alias ywdsem '~/ywdsem/upload_to_git.sh'
--- dotfiles/.config/nvim/lua/plugins/noice.lua
+++ dotfiles/.config/nvim/lua/plugins/noice.lua
@@ -1,5 +1,6 @@
 return {
 	"folke/noice.nvim",
+	---@type noice.Config
 	event = "VeryLazy",
 	opts = {
 		-- add any options here
@@ -11,11 +12,6 @@ return {
 		},
 	},
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
 	},
 }

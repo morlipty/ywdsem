c59c2ed3e8efa648243db769dbb47af1e37d8710
morlipty <morlipty@proton.me>
2025-10-16 15:26:22 +0400

    Backup 2025:10:16-15:26

--- dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
+++ dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,4 +1,17 @@
 return {
 	"ibhagwan/fzf-lua",
-	opts = {},
+	config = function()
+		local fzflua = require("fzf-lua")
+
+		vim.keymap.set("n", "<leader>ff", function()
+			fzflua.builtin()
+		end, { desc = "Search" })
+
+		vim.keymap.set("n", "<leader>ft", function()
+			fzflua.grep_project({
+				search = "TODO",
+				rg_opts = [[--column --line-number --no-heading --color=always --smart-case --hidden -g '!.git' -g '!fzf-lua.lua']],
+			})
+		end, { desc = "Search TODO" })
+	end,
 }

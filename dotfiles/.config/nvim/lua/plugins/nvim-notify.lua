964ac2e589ae3bfe5303a2e7128b6ee73807607a
morlipty <morlipty@proton.me>
2025-10-17 13:14:54 +0400

    Backup 2025:10:17-13:14

--- dotfiles/.config/nvim/lua/plugins/nvim-notify.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-notify.lua
@@ -1,7 +1,6 @@
 return {
 	"rcarriga/nvim-notify",
 	config = function()
-		local async = require("plenary.async")
-		vim.notify = require("notify").async
+		vim.notify = require("notify")
 	end,
 }
--- dotfiles/.config/nvim/tes.py
+++ dotfiles/.config/nvim/tes.py
@@ -1,2 +1,2 @@
-def hello():
-    pass
+exit
+

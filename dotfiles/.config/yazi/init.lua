425055bb633a625c029cfa037ff48819966b6e78
morlipty <morlipty@proton.me>
2025-10-20 21:29:45 +0400

    Backup 2025:10:20-21:29

--- dotfiles/.config/yazi/init.lua
+++ dotfiles/.config/yazi/init.lua
@@ -1,1 +1,7 @@
 require("full-border"):setup()
+
+-- function Linemode:size_and_permissions()
+-- 	local size = self._file:size()
+-- 	local permissions = self._file:permissions()
+-- 	return string.format("%s %s", size and ya.readable_size(size) or "-", permissions)
+-- end

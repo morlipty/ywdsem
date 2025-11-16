309e7788b239fbb0d1c4c631e0bf5b834ef9a732
morlipty <morlipty@proton.me>
2025-11-16 21:30:53 +0400

    Add treesitter highlighting + close by q

--- dotfiles/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/.config/nvim/lua/config/autocmds.lua
@@ -10,19 +10,48 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- Enable folding
-local ts_fold_group = vim.api.nvim_create_augroup("treesitter_folds", { clear = true })
+-- Enable Tree-sitter
+local ts_group = vim.api.nvim_create_augroup("ts_highlighting_and_folding", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-	group = ts_fold_group,
-	desc = "Enable Tree-sitter folding when parser is available",
-	callback = function()
-		if pcall(vim.treesitter.get_parser, 0) then
-			vim.wo.foldlevel = 99
-			vim.wo.foldmethod = "expr"
-			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		else
-			vim.wo.foldmethod = "manual"
+	group = ts_group,
+	desc = "Enable Tree-sitter highlighting and folding when parser is available",
+	callback = function(args)
+		local ok = pcall(vim.treesitter.start, args.buf)
+
+		if not ok then
+			vim.opt_local.foldmethod = "manual"
+			return
 		end
+
+		vim.opt_local.foldlevel = 99
+		vim.opt_local.foldmethod = "expr"
+		vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+	end,
+})
+
+-- Close some filetypes with q
+local close_with_q_group = vim.api.nvim_create_augroup("clear_with_q", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = close_with_q_group,
+	pattern = {
+		"notify",
+		"checkhealth",
+		"lspinfo",
+	},
+
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			vim.keymap.set("n", "q", function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+			end, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+		end)
 	end,
 })

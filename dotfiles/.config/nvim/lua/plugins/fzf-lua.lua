fc77ad90b5ddb453f938e781e8cbf0a3c91e1e56
morlipty <morlipty@proton.me>
2025-11-16 21:31:12 +0400

    Change keybindings

--- dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
+++ dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
@@ -8,19 +8,23 @@ return {
 			fzf_colors = true,
 		})
 
-		vim.keymap.set("n", "<leader>ff", function()
+		vim.keymap.set("n", "\\\\", function()
 			fzflua.builtin()
 		end, { desc = "Search" })
 
-		vim.keymap.set("n", "<leader>fb", function()
+		vim.keymap.set("n", "\\b", function()
 			fzflua.buffers()
 		end, { desc = "Buffers" })
 
-		vim.keymap.set("n", "<leader>fd", function()
+		vim.keymap.set("n", "\\f", function()
+			fzflua.files()
+		end, { desc = "Files" })
+
+		vim.keymap.set("n", "\\d", function()
 			fzflua.diagnostics_document()
 		end, { desc = "Documents Diagnostics" })
 
-		vim.keymap.set("n", "<leader>fD", function()
+		vim.keymap.set("n", "\\D", function()
 			fzflua.diagnostics_workspace()
 		end, { desc = "Workspace Diagnostics" })
 	end,

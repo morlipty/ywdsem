65bf195cb74604b9b3d49587ac58718d69f1c209
morlipty <morlipty@proton.me>
2025-11-18 22:52:15 +0400

    Separate blink and luasnip

--- dotfiles/.config/nvim/lua/plugins/blink.lua
+++ dotfiles/.config/nvim/lua/plugins/blink.lua
@@ -1,70 +1,54 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
+	"saghen/blink.cmp",
 
-		verison = "v2.*",
-		build = "make install_jsregexp",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
+	version = "1.*",
 
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+	config = function()
+		local blink = require("blink.cmp")
+		blink.setup({
+			snippets = { preset = "luasnip" },
+			keymap = { preset = "default" },
 
-	{
-		"saghen/blink.cmp",
+			appearance = {
+				nerd_font_variant = "mono",
+			},
 
-		version = "1.*",
-
-		config = function()
-			local blink = require("blink.cmp")
-			blink.setup({
-				snippets = { preset = "luasnip" },
-				keymap = { preset = "default" },
-
-				appearance = {
-					nerd_font_variant = "mono",
-				},
-
-				sources = {
-					default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-					providers = {
-						lazydev = {
-							name = "LazyDev",
-							module = "lazydev.integrations.blink",
-							score_offset = 100,
-						},
+			sources = {
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
 					},
 				},
+			},
 
-				cmdline = {
-					completion = {
-						menu = { auto_show = true },
-					},
-				},
-
+			cmdline = {
 				completion = {
+					menu = { auto_show = true },
+				},
+			},
 
-					menu = {
-						draw = {
-							treesitter = { "lsp" },
-							columns = {
-								{ "kind_icon" },
-								{ "label", "label_description", gap = 1 },
-								{ "kind" },
-								{ "source_name" },
-							},
+			completion = {
+
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+						columns = {
+							{ "kind_icon" },
+							{ "label", "label_description", gap = 1 },
+							{ "kind" },
+							{ "source_name" },
 						},
 					},
-					documentation = {
-						auto_show = true,
-						auto_show_delay_ms = 200,
-					},
 				},
-				fuzzy = { implementation = "prefer_rust_with_warning" },
-			})
-		end,
-	},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		})
+	end,
 }

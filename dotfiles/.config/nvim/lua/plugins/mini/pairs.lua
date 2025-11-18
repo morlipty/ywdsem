fb360c36a929576a1b338d714bfedfd85d34c645
morlipty <morlipty@proton.me>
2025-11-18 22:47:45 +0400

    Delete unused mini module files

--- dotfiles/.config/nvim/lua/plugins/mini/pairs.lua
+++
@@ -1,4 +0,0 @@
-return function()
-	local pairs = require("mini.pairs")
-	pairs.setup({})
-end
--- dotfiles/.config/nvim/lua/plugins/mini/statusline.lua
+++
@@ -1,6 +0,0 @@
-return function()
-	local statusline = require("mini.statusline")
-	statusline.setup({
-		content = { active = active },
-	})
-end

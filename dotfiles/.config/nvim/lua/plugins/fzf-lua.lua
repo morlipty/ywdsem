fbaf19201fdd0f4b7a8baca02eb3874fe680d603
morlipty <morlipty@proton.me>
2025-11-22 16:05:36 +0400

    Add some useful shortcuts

--- dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
+++ dotfiles/.config/nvim/lua/plugins/fzf-lua.lua
@@ -27,5 +27,25 @@ return {
 		vim.keymap.set("n", "\\D", function()
 			fzflua.diagnostics_workspace()
 		end, { desc = "Workspace Diagnostics" })
+
+		vim.keymap.set("n", "\\l", function()
+			fzflua.live_grep()
+		end, { desc = "Live grep search" })
+
+		vim.keymap.set("n", "\\c", function()
+			fzflua.grep_curbuf()
+		end, { desc = "Current buf grep search" })
+
+		vim.keymap.set("n", "\\s", function()
+			fzflua.spell_suggest()
+		end, { desc = "Spell suggest" })
+
+		vim.keymap.set("n", "\\t", function()
+			fzflua.grep({
+				search = "TODO|FIXME|HACK|NOTE",
+				cmd = "rg --color=always --smart-case -g '!{.git}/' -g '!mini/hipatterns.lua' -g '!fzf-lua.lua'",
+				no_esc = true,
+			})
+		end, { desc = "Search for TODOS etc" })
 	end,
 }

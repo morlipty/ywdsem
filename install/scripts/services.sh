444dca91d296d044a409ff0810679da3ba7cc0ac
morlipty <morlipty@proton.me>
2026-01-02 22:14:55 +0400

    Add Services

--- install/scripts/services.sh
+++ install/scripts/services.sh
@@ -5,10 +5,16 @@ figlet -f smslant "Enable services"
 echo -e "${NONE}"
 
 Services=(
+  "keyd"
   "sddm"
+  "bluetooth"
   "power-profiles-daemon"
+  "dnscrypt-proxy"
+  "firewalld"
   "thermald"
+  "fstrim"
   "paccache.timer"
+  "pkgfile-update.timer"
 )
 
 systemctl enable "${Services[@]}"

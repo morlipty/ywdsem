363709cbef4084eb6e9d4eeb4e5524309add668f
morlipty <morlipty@proton.me>
2026-01-29 19:23:00 +0400

    Add check on empty lines

--- dotfiles/.config/fish/functions/fixlrc.fish
+++ dotfiles/.config/fish/functions/fixlrc.fish
@@ -1,5 +1,5 @@
 function fixlrc
-    set -l pat '^[[:space:]]*\[(ar|by|al|ti|offset|re|ve|length):.*\][[:space:]]*$'
+    set -l pat '^[[:space:]]*\[(ar|by|al|ti|offset|re|ve|length):.*\][[:space:]]*$|^[[:space:]]*$'
 
     echo "Files that WOULD be changed:"
     fd -e lrc -x rg -l -i $pat
@@ -11,7 +11,9 @@ function fixlrc
 
     read -P "Apply deletion? (y/N) " ans
     if test "$ans" = y; or test "$ans" = Y
-        fd -e lrc -x sed -i '/^[[:space:]]*\[\(ar\|by\|al\|ti\|offset\|re\|ve\|length\):.*\][[:space:]]*$/Id'
+        fd -e lrc -x sed -i \
+          -e '/^[[:space:]]*\[\(ar\|by\|al\|ti\|offset\|re\|ve\|length\):.*\][[:space:]]*$/Id' \
+          -e '/^[[:space:]]*$/d'
         echo "Done."
     else
         echo "Cancelled."

79457020fc150d92b324718839421e54af03e00a
morlipty <morlipty@proton.me>
2026-01-31 21:05:20 +0400

    Separate lsp and lsp_mapping

--- dotfiles/.config/nvim/lua/config/lsp.lua
+++ dotfiles/.config/nvim/lua/config/lsp.lua
@@ -37,34 +37,3 @@ vim.diagnostic.config({
 		},
 	},
 })
-
--- Keymaps
--- Hover on buffer
-vim.keymap.set("n", "<leader>lh", function()
-	vim.lsp.buf.hover()
-end, { desc = "Hover lsp" })
-
--- LSP rename functionality
-vim.keymap.set("n", "<leader>lr", function()
-	vim.lsp.buf.rename()
-end, { desc = "Rename" })
-
--- Toggle virtual text with virtual lines
--- Modes: 1 = text only, 2 = lines only, 3 = off
-local diag_mode = 2
-
-vim.keymap.set("n", "<leader>tl", function()
-	if diag_mode == 1 then
-		vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
-		vim.notify("Diagnostics: virtual text")
-		diag_mode = 2
-	elseif diag_mode == 2 then
-		vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
-		vim.notify("Diagnostics: virtual lines")
-		diag_mode = 3
-	else
-		vim.diagnostic.config({ virtual_text = false, virtual_lines = false })
-		vim.notify("Diagnostics: disabled")
-		diag_mode = 1
-	end
-end, { desc = "Cycle diagnostic display mode" })

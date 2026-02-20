69aec34f71dcea848cce150125c91ccf205da452
morlipty <morlipty@proton.me>
2026-02-20 18:45:05 +0400

    feat(fish): change music aliases into the function

--- dotfiles/.config/fish/conf.d/aliases.fish
+++
@@ -1,3 +0,0 @@
-#music
-alias musicsampleratecheck 'find ~/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate,bit_depth -of csv=p=0 {} \; | sort | uniq -c'
-alias musicalbumartrescheck 'find ~/Music -type f -iname "*.flac" -exec ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 {} \; | sort | uniq -c'

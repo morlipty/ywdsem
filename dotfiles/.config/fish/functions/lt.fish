4a229721eb8691608b8f28b6d298a42735482daa
morlipty <morlipty@proton.me>
2026-02-20 17:23:48 +0400

    chore(fish): delete lt function

--- dotfiles/.config/fish/functions/lt.fish
+++
@@ -1,5 +0,0 @@
-function lt --description 'eza tree with optional level (default 1)'
-  set -l level 1
-  set -q argv[1]; and set level $argv[1]
-  eza -a --tree --level=$level --icons=always $argv[2..-1]
-end

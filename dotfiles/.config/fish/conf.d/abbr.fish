450bffcbf3dca9d5b53e6c5c4736a694cada2696
morlipty <morlipty@proton.me>
2026-02-20 20:30:45 +0400

    feat(fish): remove pkglist function in favor of abbrs

--- dotfiles/.config/fish/conf.d/abbr.fish
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -3,6 +3,8 @@ abbr e 'eza -a'
 abbr el 'eza -al'
 
 #package manager
+abbr pkglist 'paru -Qq | fzf --style full --preview "paru -Qi {}; rg \'(installed|upgraded|removed) {} \' /var/log/pacman.log"'
+abbr pkghist 'rg "(installed|upgraded|removed)" /var/log/pacman.log | fzf'
 abbr reflector-update 'sudo reflector --verbose --country Iran,Germany,France --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
 abbr pacunlock 'sudo rm -i /var/lib/pacman/db.lck'
 

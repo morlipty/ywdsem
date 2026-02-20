a013ab64b42438bd2eb042e0d0e598284db64e60
morlipty <morlipty@proton.me>
2026-02-20 17:58:25 +0400

    feat(fish): add eza abbrs

--- dotfiles/.config/fish/conf.d/abbr.fish
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -1,7 +1,11 @@
-#grub
-abbr grub-update 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
-abbr grub-ebr 'sudo grub-install --efi-directory=/efi --boot-directory=/boot --removable'
+#general
+abbr e 'eza -a'
+abbr el 'eza -al'
 
 #package manager
 abbr reflector-update 'sudo reflector --verbose --country Iran,Germany,France --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
 abbr pacunlock 'sudo rm -i /var/lib/pacman/db.lck'
+
+#grub
+abbr grub-update 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
+abbr grub-ebr 'sudo grub-install --efi-directory=/efi --boot-directory=/boot --removable'

bb2a4da9d7600c1741804dfe9ca6e1087199452d
morlipty <morlipty@proton.me>
2026-02-20 17:25:05 +0400

    refactor(fish): remove unneded aliases and move some to abbrs

--- dotfiles/.config/fish/conf.d/abbr.fish
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -1,3 +1,7 @@
 #grub
 abbr grub-update 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
 abbr grub-ebr 'sudo grub-install --efi-directory=/efi --boot-directory=/boot --removable'
+
+#package manager
+abbr reflector-update 'sudo reflector --verbose --country Iran,Germany,France --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
+abbr pacunlock 'sudo rm -i /var/lib/pacman/db.lck'

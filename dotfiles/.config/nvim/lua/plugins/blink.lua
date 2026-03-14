8940ec072bcccf013736baf592538c8ea6a56634
morlipty <morlipty@proton.me>
2026-03-14 22:20:23 +0400

    feat(nvim): add lazydev

--- dotfiles/.config/nvim/lua/plugins/blink.lua
+++ dotfiles/.config/nvim/lua/plugins/blink.lua
@@ -26,6 +26,16 @@ return {
           'snippets',
           'buffer',
         },
+        per_filetype = {
+          lua = { inherit_defaults = true, 'lazydev' },
+        },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+        },
       },
 
       completion = {

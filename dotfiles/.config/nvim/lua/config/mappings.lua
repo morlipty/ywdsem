c34a2798de24a8162b93e987b5a621be70930af6
morlipty <morlipty@proton.me>
2026-03-16 20:21:30 +0400

    feat(nvim): add mapping to set current working directory and change nowrap -> wrapping

--- dotfiles/.config/nvim/lua/config/mappings.lua
+++ dotfiles/.config/nvim/lua/config/mappings.lua
@@ -27,4 +27,11 @@ vk.set('n', '<Esc>', '<cmd>noh<CR>')
 -- Toggle wrapping
 vk.set('n', '<leader>tw', function()
   vim.opt.wrap = not vim.opt.wrap
-end, { desc = 'Toggle nowrap' })
+end, { desc = 'Toggle wrapping' })
+
+-- Change current working directory
+vk.set('n', '<leader>sc', function()
+  local file_dir = vim.fn.expand('%:p:h')
+  vim.cmd('cd ' .. file_dir)
+  vim.notify('cwd set to ' .. file_dir)
+end, { desc = 'Set cwd' })

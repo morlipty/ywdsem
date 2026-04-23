b7b8f009adc0dde71ce030b38f2c10947ae6f6cb
morlipty <morlipty@proton.me>
2026-04-23 21:13:34 +0400

    nvim: remove treesitter autocmd

--- dotfiles/.config/nvim/plugin/treesitter.lua
+++ dotfiles/.config/nvim/plugin/treesitter.lua
@@ -19,15 +19,3 @@ treesitter.install({
   'rust',
   'ron',
 })
-
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(args)
-    local name, kind = args.data.spec.name, args.data.kind
-    if name == 'nvim-treesitter' and kind == 'update' then
-      if not args.data.active then
-        vim.cmd.packadd('nvim-treesitter')
-      end
-      vim.cmd('TSUpdate')
-    end
-  end,
-})

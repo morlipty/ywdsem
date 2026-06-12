86a504ff977d620b710b48503a7f943ff5441ce2
morlipty <morlipty@proton.me>
2026-06-12 10:31:19 +0400

    nvim: formatting

--- dotfiles/.config/nvim/lua/lsp.lua
+++ dotfiles/.config/nvim/lua/lsp.lua
@@ -14,23 +14,28 @@ vim.lsp.enable({
   -- Oxfmt
   'oxfmt',
   -- Github actions
-  'gh_actions_ls'
+  'gh_actions_ls',
 })
 
 -- Diagnostics configuration
-local signs = {
-  [vim.diagnostic.severity.ERROR] = '󰅚 ',
-  [vim.diagnostic.severity.WARN] = '󰀪 ',
-  [vim.diagnostic.severity.INFO] = '󰋽 ',
-  [vim.diagnostic.severity.HINT] = '󰌶 ',
-}
-
 vim.diagnostic.config({
   virtual_lines = false,
-  virtual_text = { spacing = 2, prefix = '●' },
+  virtual_text = {
+    spacing = 2,
+    prefix = '●',
+  },
   underline = true,
   update_in_insert = false,
   severity_sort = true,
-  float = { source = true },
-  signs = { text = signs },
+  float = {
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  },
 })

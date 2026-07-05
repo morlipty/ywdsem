2122ccf62ed33deb4513b23762a4927ab914357e
morlipty <morlipty@proton.me>
2026-07-05 18:52:42 +0400

    nvim: fix basedpyright noisy notifications

--- dotfiles/.config/nvim/lsp/basedpyright.lua
+++ dotfiles/.config/nvim/lsp/basedpyright.lua
@@ -7,6 +7,16 @@ return {
       },
     },
   },
+  handlers = {
+    --- filter noisy notifications
+    ['$/progress'] = function(err, result, ctx)
+      -- just notify once
+      if result.token == (vim.g.basedpyright_progress_token or result.token) then
+        vim.g.basedpyright_progress_token = result.token
+        vim.lsp.handlers['$/progress'](err, result, ctx)
+      end
+    end,
+  },
   filetypes = {
     'python',
   },

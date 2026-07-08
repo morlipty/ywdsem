ae746c0ab0b2a3350d9fb1279f587f46cb69958c
morlipty <morlipty@proton.me>
2026-07-08 23:31:46 +0400

    nvim: statusline improvements

--- dotfiles/.config/nvim/lua/statusline.lua
+++ dotfiles/.config/nvim/lua/statusline.lua
@@ -1,50 +1,50 @@
 local lsp = {}
 vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach' }, {
   callback = vim.schedule_wrap(function(args)
-    local clients = vim.lsp.get_clients({ bufnr = args.buf })
+    local buf = args.buf
+    local clients = vim.lsp.get_clients({ bufnr = buf })
     if #clients > 0 then
       local names = {}
       for i = 1, #clients do
         names[i] = clients[i].name
       end
-      lsp[args.buf] = ' ' .. table.concat(names, ' ')
+      lsp[buf] = ' ' .. table.concat(names, ' ') .. ' '
     else
-      lsp[args.buf] = nil
+      lsp[buf] = nil
     end
   end),
 })
 
-local modes = {
-  ['n'] = 'NORMAL',
-  ['v'] = 'VISUAL',
-  ['V'] = 'V-LINE',
-  ['\22'] = 'V-BLOCK',
-  ['s'] = 'SELECT',
-  ['S'] = 'S-LINE',
-  ['\19'] = 'S-BLOCK',
-  ['i'] = 'INSERT',
-  ['R'] = 'REPLACE',
-  ['c'] = 'COMMAND',
-  ['r'] = 'PROMPT',
-  ['!'] = 'SHELL',
-  ['t'] = 'TERMINAL',
-}
-
-local current_mode = ''
-vim.api.nvim_create_autocmd({ 'VimEnter', 'ModeChanged' }, {
-  callback = function()
-    local m = vim.api.nvim_get_mode().mode
-    current_mode = '| ' .. (modes[m] or m) .. ' | '
+vim.api.nvim_create_autocmd({ 'BufDelete', 'BufWipeout' }, {
+  callback = function(args)
+    lsp[args.buf] = nil
   end,
 })
 
+local modes = {
+  ['n'] = '| NORMAL | ',
+  ['v'] = '| VISUAL | ',
+  ['V'] = '| V-LINE | ',
+  ['\22'] = '| V-BLOCK | ',
+  ['s'] = '| SELECT | ',
+  ['S'] = '| S-LINE | ',
+  ['\19'] = '| S-BLOCK | ',
+  ['i'] = '| INSERT | ',
+  ['R'] = '| REPLACE | ',
+  ['c'] = '| COMMAND | ',
+  ['r'] = '| PROMPT | ',
+  ['!'] = '| SHELL | ',
+  ['t'] = '| TERMINAL | ',
+}
+
 function Statusline()
-  return current_mode
+  local m = vim.api.nvim_get_mode().mode
+  return (modes[m] or m)
     .. (vim.b.minidiff_summary_string or '')
     .. '%=%F %r%m%h%='
     .. vim.diagnostic.status()
     .. (lsp[vim.api.nvim_get_current_buf()] or '')
-    .. ' %y'
+    .. '%y'
 end
 
 vim.o.statusline = '%{%v:lua.Statusline()%}'

7ed678a68a96be619afc152c82a8f66dacc5a2b4
morlipty <morlipty@proton.me>
2026-07-09 16:03:07 +0400

    nvim: add highlight groups for statusline

--- dotfiles/.config/nvim/plugin/catppuccin.lua
+++ dotfiles/.config/nvim/plugin/catppuccin.lua
@@ -11,9 +11,31 @@ catppuccin.setup({
     transparent = true,
     solid = false,
   },
-  custom_highlights = function(colors)
+  custom_highlights = function(c)
     return {
-      BlinkCmpMenuBorder = { bg = colors.none },
+      BlinkCmpMenuBorder = { bg = c.none },
+
+      -- Normal
+      StlNormal = { bg = c.blue, fg = c.mantle, bold = true },
+      StlNormalInv = { fg = c.blue, bg = c.none },
+      -- Insert
+      StlInsert = { fg = c.base, bg = c.green, bold = true },
+      StlInsertInv = { fg = c.green, bg = c.none },
+      -- Terminal
+      StlTerminal = { fg = c.base, bg = c.green, bold = true },
+      StlTerminalInv = { fg = c.green, bg = c.none },
+      -- Command
+      StlCommand = { fg = c.base, bg = c.peach, bold = true },
+      StlCommandInv = { fg = c.peach, bg = c.none },
+      -- Visual
+      StlVisual = { fg = c.base, bg = c.mauve, bold = true },
+      StlVisualInv = { fg = c.mauve, bg = c.none },
+      -- Replace
+      StlReplace = { fg = c.base, bg = c.red, bold = true },
+      StlReplaceInv = { fg = c.red, bg = c.none },
+      -- Inactive
+      StlInactive = { fg = c.surface1, bg = c.none, bold = true },
+      StlInactiveInv = { fg = c.surface1, bg = c.none },
     }
   end,
   integrations = {

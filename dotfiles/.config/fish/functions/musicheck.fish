92134ff1b1f84f752da6d621acf26a7ec8d4e7fc
morlipty <morlipty@proton.me>
2026-07-13 23:35:18 +0400

    fish: revamp musicheck function

--- dotfiles/.config/fish/functions/musicheck.fish
+++ dotfiles/.config/fish/functions/musicheck.fish
@@ -1,21 +1,45 @@
-function musicheck
-    for arg in $argv
-        switch $arg
-            case --audio
-                fd --extension flac . ~/Music --print0 |
-                    parallel -0 ffprobe -v error -select_streams a:0 \
-                        -show_entries stream=sample_rate,bit_depth \
-                        -of csv=p=0 {} |
-                    sort | uniq -c
-            case --art
-                fd --extension flac . ~/Music --print0 |
-                    parallel -0 ffprobe -v error -select_streams v:0 \
-                        -show_entries stream=width,height \
-                        -of csv=p=0 {} |
-                    sort | uniq -c
-            case "*"
-                echo "Unknown option: $arg"
-                return 1
-        end
+function musicheck --description "Summarize audio or art properties of FLAC files"
+    argparse h/help a/audio r/art -- $argv
+    or return 1
+
+    if set -q _flag_help
+        echo "Usage: musicheck (--audio | --art)"
+        return 0
     end
+
+    # --- pick mode ---
+    set -l stream
+    set -l entries
+
+    if set -q _flag_audio
+        set stream a:0
+        set entries stream=sample_rate,bit_depth
+    else if set -q _flag_art
+        set stream v:0
+        set entries stream=width,height
+    else
+        echo "musicheck: specify --audio or --art" >&2
+        return 1
+    end
+
+    # --- scan ---
+    set -l files (fd --extension flac . ~/Music)
+    set -l total (count $files)
+    set -l i 0
+
+    for file in $files
+        set i (math $i + 1)
+        printf "\rScanning %d/%d" $i $total >&2
+
+        set -l result (ffprobe -v error -select_streams $stream \
+            -show_entries $entries -of csv=p=0 $file)
+
+        if test -z "$result"
+            echo MISSING
+        else
+            echo $result
+        end
+    end | sort | uniq -c | sort -rn
+
+    echo >&2
 end

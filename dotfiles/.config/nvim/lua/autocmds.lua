f5b0d711b61a8aa4dac3d1b4f680b2085fd37413
morlipty <morlipty@proton.me>
2026-07-19 20:35:48 +0400

    nvim: use autocmd alias and add autocmd to restore cursor position after reopening file

--- dotfiles/.config/nvim/lua/autocmds.lua
+++ dotfiles/.config/nvim/lua/autocmds.lua
@@ -1,12 +1,12 @@
 local api = vim.api
+local autocmd = api.nvim_create_autocmd
 
--- General function to create group names
 local function augroup(name)
   return api.nvim_create_augroup(name, { clear = true })
 end
 
 -- Highlight text on yank
-api.nvim_create_autocmd('TextYankPost', {
+autocmd('TextYankPost', {
   group = augroup('highlight_text_on_yank'),
   callback = function()
     vim.hl.on_yank({ timeout = 400 })
@@ -14,12 +14,24 @@ api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Enable Tree-sitter
-api.nvim_create_autocmd('FileType', {
+autocmd('FileType', {
   group = augroup('treesitter'),
   callback = function(ev)
     if pcall(vim.treesitter.start, ev.buf) then
       vim.wo[0][0].foldmethod = 'expr'
       vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    end
+  end,
+})
+
+-- Restore cursor position after reopening file
+autocmd('BufReadPost', {
+  group = augroup('last_location'),
+  callback = function(ev)
+    local mark = api.nvim_buf_get_mark(ev.buf, '"')
+    local line_count = api.nvim_buf_line_count(ev.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd('normal! g`"zz')
     end
   end,
 })

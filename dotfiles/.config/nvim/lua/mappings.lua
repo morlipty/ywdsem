01fd555eef23760c849d03fd7dfef263e3c08a43
morlipty <morlipty@proton.me>
2026-07-20 20:05:07 +0400

    nvim: introduce map_toggle function

--- dotfiles/.config/nvim/lua/mappings.lua
+++ dotfiles/.config/nvim/lua/mappings.lua
@@ -41,17 +41,31 @@ end, {
   desc = 'Visually select changed text',
 })
 
--- Toggle wrapping
-map('n', '<leader>tw', function()
-  vim.wo.wrap = not vim.wo.wrap
-end, { desc = 'Toggle line wrapping' })
+local function map_toggle(key, name, toggle)
+  map('n', '<leader>t' .. key, function()
+    local is_enabled
+    if type(toggle) == 'string' then
+      is_enabled = not vim.wo[toggle]
+      vim.wo[toggle] = is_enabled
+    elseif type(toggle) == 'function' then
+      is_enabled = toggle()
+    end
+    vim.notify(('%s: %s'):format(name, is_enabled and 'On' or 'Off'))
+  end, { desc = 'Toggle ' .. name })
+end
 
--- Toggle inlay hints
-map('n', '<leader>th', function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = 'Toggle LSP inlay hints' })
+map_toggle('w', 'Wrapping', 'wrap')
+map_toggle('s', 'Spell checking', 'spell')
 
--- Toggle codelens
-map('n', '<leader>tc', function()
-  vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled())
-end, { desc = 'Toggle codelens' })
+map_toggle('i', 'LSP inlay hints', function()
+  local inlay_hint = vim.lsp.inlay_hint
+  local is_enabled = not inlay_hint.is_enabled({ bufnr = 0 })
+  inlay_hint.enable(is_enabled, { bufnr = 0 })
+  return is_enabled
+end)
+map_toggle('c', 'Codelens', function()
+  local codelens = vim.lsp.codelens
+  local is_enabled = not codelens.is_enabled({ bufnr = 0 })
+  codelens.enable(is_enabled, { bufnr = 0 })
+  return is_enabled
+end)

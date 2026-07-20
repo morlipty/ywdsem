d9078875b339ae77ee1c371925a86a4a513bbde9
morlipty <morlipty@proton.me>
2026-07-20 20:03:33 +0400

    nvim: run lazydev autocmd once

--- dotfiles/.config/nvim/plugin/lazydev.lua
+++ dotfiles/.config/nvim/plugin/lazydev.lua
@@ -2,6 +2,7 @@ vim.pack.add({ 'https://github.com/folke/lazydev.nvim' })
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'lua',
+  once = true,
   callback = function()
     local lazydev = require('lazydev')
 

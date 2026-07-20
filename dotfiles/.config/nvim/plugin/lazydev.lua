e1663546b492cd09fac154c103767e178a213630
morlipty <morlipty@proton.me>
2026-07-20 13:50:32 +0400

    nvim: use lazydev for faster plugin files auto-completion

---
+++ dotfiles/.config/nvim/plugin/lazydev.lua
@@ -0,0 +1,15 @@
+vim.pack.add({ 'https://github.com/folke/lazydev.nvim' })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'lua',
+  callback = function()
+    local lazydev = require('lazydev')
+
+    lazydev.setup({
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'mini.nvim', words = { 'Mini' } },
+      },
+    })
+  end,
+})

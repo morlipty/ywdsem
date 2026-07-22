a93d1cd394d049c5a80413c9694a85c597a5eef6
morlipty <morlipty@proton.me>
2026-07-22 18:24:23 +0400

    fish: add abbr for system updates

--- dotfiles/.config/fish/conf.d/abbr.fish
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -5,6 +5,7 @@ abbr -a --set-cursor='%' -- e 'eza --icons -a%'
 abbr -a --set-cursor='%' -- el 'eza --icons --header --git --loc -al%'
 
 #package manager
+abbr -a -- yu 'yay -Pw && yay'
 abbr -a --set-cursor='%' -- pkglist 'pacman -Qq% | fzf --style full --preview "pacman -Qi {}"'
 abbr -a -- pkghist 'tspin /var/log/pacman.log | rg "(installed|removed|upgraded)" | tac | fzf --ansi --no-sort'
 abbr -a -- pacunlock 'sudo rm -i /var/lib/pacman/db.lck'

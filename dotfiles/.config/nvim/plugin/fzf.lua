2d769804991b921142202e3db71f5e96a2e63e02
morlipty <morlipty@proton.me>
2026-07-24 19:11:18 +0400

    nvim: fzf-lua refactor, add git commands

--- dotfiles/.config/nvim/plugin/fzf.lua
+++ dotfiles/.config/nvim/plugin/fzf.lua
@@ -3,49 +3,42 @@ vim.pack.add({ 'https://github.com/ibhagwan/fzf-lua' })
 local map = vim.keymap.set
 
 local fzf = require('fzf-lua')
+local actions = fzf.actions
 
 fzf.setup({
   fzf_opts = {
     ['--layout'] = 'default',
   },
+  git = {
+    status = {
+      actions = {
+        ['right'] = false,
+        ['left'] = false,
+        ['ctrl-h'] = { fn = actions.git_stage, reload = true },
+        ['ctrl-l'] = { fn = actions.git_unstage, reload = true },
+      },
+    },
+  },
   ui_select = {},
 })
 
-map('n', '\\\\', function()
-  fzf.builtin()
-end, { desc = 'Search' })
+-- General
+map('n', '\\\\', fzf.builtin, { desc = 'FzfLua' })
+map('n', '\\b', fzf.buffers, { desc = 'Buffers' })
+map('n', '\\j', fzf.jumps, { desc = 'Jumps' })
+map('n', '\\f', fzf.files, { desc = 'Files' })
+map('n', '\\o', fzf.oldfiles, { desc = 'Oldfiles' })
+map('n', '\\l', fzf.live_grep, { desc = 'Live grep search' })
+map('n', '\\c', fzf.grep_curbuf, { desc = 'Current buf grep search' })
+map('n', '\\s', fzf.spell_suggest, { desc = 'Spell suggest' })
+map('n', '\\d', fzf.diagnostics_document, { desc = 'Documents Diagnostics' })
+map('n', '\\D', fzf.diagnostics_workspace, { desc = 'Workspace Diagnostics' })
 
-map('n', '\\b', function()
-  fzf.buffers()
-end, { desc = 'Buffers' })
-
-map('n', '\\f', function()
-  fzf.files()
-end, { desc = 'Files' })
-
-map('n', '\\o', function()
-  fzf.oldfiles()
-end, { desc = 'Oldfiles' })
-
-map('n', '\\d', function()
-  fzf.diagnostics_document()
-end, { desc = 'Documents Diagnostics' })
-
-map('n', '\\D', function()
-  fzf.diagnostics_workspace()
-end, { desc = 'Workspace Diagnostics' })
-
-map('n', '\\l', function()
-  fzf.live_grep()
-end, { desc = 'Live grep search' })
-
-map('n', '\\c', function()
-  fzf.grep_curbuf()
-end, { desc = 'Current buf grep search' })
-
-map('n', '\\s', function()
-  fzf.spell_suggest()
-end, { desc = 'Spell suggest' })
+-- Git
+map('n', '\\gs', fzf.git_status, { desc = 'Git status' })
+map('n', '\\gf', fzf.git_files, { desc = 'Git files' })
+map('n', '\\gc', fzf.git_commits, { desc = 'Git commits' })
+map('n', '\\gb', fzf.git_bcommits, { desc = 'Git bcommits' })
 
 map('n', '\\t', function()
   fzf.grep({

4e9215e7f072dab7ba574d48b35a111cee3611da
morlipty <morlipty@proton.me>
2026-07-28 22:40:02 +0400

    fish: remove explicit '%' cursor placeholder

--- dotfiles/.config/fish/conf.d/abbr.fish
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -1,10 +1,10 @@
 #general
 abbr -a -- nv nvim
-abbr -a --set-cursor='%' -- e 'eza --icons -a%'
-abbr -a --set-cursor='%' -- el 'eza --icons --header --git --loc -al%'
+abbr -a --set-cursor -- e 'eza --icons -a%'
+abbr -a --set-cursor -- el 'eza --icons --header --git --loc -al%'
 
 #package manager
 abbr -a -- yu 'yay -Pw && yay'
 abbr -a -- pacunlock 'sudo rm -i /var/lib/pacman/db.lck'
-abbr -a --set-cursor='%' -- pkglist 'pacman -Qq% | fzf --style full --preview "pacman -Qi {}"'
-abbr -a --set-cursor='%' -- pkghist 'tspin /var/log/pacman.log | rg --color=always "(installed|removed|upgraded%)" | tac | fzf --ansi --no-sort'
+abbr -a --set-cursor -- pkglist 'pacman -Qq% | fzf --style full --preview "pacman -Qi {}"'
+abbr -a --set-cursor -- pkghist 'tspin /var/log/pacman.log | rg --color=always "(installed|removed|upgraded%)" | tac | fzf --ansi --no-sort'

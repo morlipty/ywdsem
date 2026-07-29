3a1c38e38d3a0e9ba25bde16a7f6ec833590c02b
morlipty <morlipty@proton.me>
2026-07-29 20:39:59 +0400

    mesa: disable vulkan video

--- dotfiles/.config/fish/conf.d/env.fish
+++ dotfiles/.config/fish/conf.d/env.fish
@@ -4,8 +4,6 @@ set -gx SUDO_EDITOR nvim
 set -gx PAGER less
 set -gx MANPAGER "nvim +Man!"
 
-set -gx ANV_DEBUG video-decode,video-encode
-
 set -gx FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
